60c903313e74ad4fa7b4c79409c97ee065ec9dba
hlburns <H.L.Burns@leeds.ac.uk>
2018-10-05 15:42:27 +0100

    chore: tidy up
    Created back ups and removed them from git
    Tidied management scripts away
    Tidied SSLfiles as using JS workaround

--- dumpPSQL.sh
+++ dumpPSQL.sh
@@ -7,7 +7,7 @@ Files created: [tableName].csv
 Users table not dumped as contains (sha-encrypted) passwords
 '
 
-psql SWIFTDB <<EOF
+psql SWIFTDB_BAK3 <<EOF
 \copy partners to 'partners.csv' csv header;
 \copy work_packages to 'work_packages.csv' csv header;
 \copy deliverables to 'deliverables.csv' csv header;

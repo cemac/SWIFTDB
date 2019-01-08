1f304dfb5b040955a0d6a33b987c3f35ac77331d
hlburns <H.L.Burns@leeds.ac.uk>
2019-01-08 13:06:56 +0000

    :beetle: fix 500 error page
    last try before I give up

--- SWIFTDBApp.py
+++ SWIFTDBApp.py
@@ -716,9 +716,15 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_error(error):
+    app.logger.error('Server Error: %s', (error))
     db.session.rollback()
     return render_template('500.html'), 500
 
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    app.logger.error('Unhandled Exception: %s', (e))
+    return render_template('500.htm'), 500
 
 if __name__ == '__main__':
     app.run()

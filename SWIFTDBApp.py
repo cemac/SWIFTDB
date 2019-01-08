d385cbe37d45ab8eaea50eaf972b8abf4dbf078b
hlburns <H.L.Burns@leeds.ac.uk>
2019-01-08 13:37:11 +0000

    :pencil2: html mispelt

--- SWIFTDBApp.py
+++ SWIFTDBApp.py
@@ -724,7 +724,7 @@ def internal_error(error):
 @app.errorhandler(Exception)
 def unhandled_exception(e):
     app.logger.error('Unhandled Exception: %s', (e))
-    return render_template('500.htm'), 500
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()

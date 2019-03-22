4a765c1654c75353a2311c0acbb9f4022377ff87
hlburns <H.L.Burns@leeds.ac.uk>
2019-03-22 15:54:58 +0000

    :globe_with_meridians: :children_crossing: making site more user friendly

--- SWIFTDBApp.py
+++ SWIFTDBApp.py
@@ -647,6 +647,7 @@ def deliverables_list():
             pass
         description = 'You are Partner Leader for: ' + ", ".join(user_partners)
     accessible_data.fillna(value="", inplace=True)
+    accessible_data.sort_values(by=["Partner"])
     data = accessible_data.drop_duplicates(keep='first', inplace=False)
     title = "Deliverables for which you are Partner Leader "
     # Set table column names:

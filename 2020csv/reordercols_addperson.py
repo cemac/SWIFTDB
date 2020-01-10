b55a50694df4071a58f079f393c960c553b46a9a
helen <h.l.burns@leeds.ac.uk>
2020-01-10 18:16:39 +0000

    :snake: handling data

--- 2020csv/reordercols_addperson.py
+++ 2020csv/reordercols_addperson.py
@@ -21,34 +21,29 @@ import pandas as pd
 m2d = pd.read_csv('month2date.tab')
 m2d = m2d.set_index('No.')
 m2d['hyphen'] = '-'
-m2d['date'] = m2d['Month'].map(str) + m2d['hyphen'].map(str) + m2d['Year'].map(str)
+m2d['date'] = m2d['Month'].map(
+    str) + m2d['hyphen'].map(str) + m2d['Year'].map(str)
 m2d.date = pd.to_datetime(m2d.date, format='%B-%Y')
 month_dic = m2d.to_dict()['date']
 file_name = ['deliverables', 'tasks']
 for f in file_name:
-    df = pd.read_csv(f + '.csv')
-    df.percent = df.percent.fillna(0).astype(int)
-    # theres some trailing white spaces
-    df.partner = df.partner.str.strip()
-    # Partners need to match existing keys
-    # make everything upper case
-    df.partner = df.partner.apply(lambda x: x.upper())
-    up = ['UOL', 'UOR', 'GMET', 'NIMET', 'UON']
-    mixp = ['UoL', 'UoR', 'GMet', 'NiMet', 'UoN']
-    for i, p in enumerate(up):
-        df.partner = df.partner.replace(p, mixp[i])
+    dfcurrent = pd.read_csv(f + '_current.csv')
+    dfcurrent['date_edited'] = '01-12-2019'
+    dfcurrent['paper_submission_date'] = ''
+    dfcurrent['person_responsible'] = ''
+    dfcurrent['previous_report'] = dfcurrent['progress']
     dfnew = pd.DataFrame()
-    dfnew = df[['id', 'code', 'work_package',
-                'description',
-                'partner',
-                'person_responsible',
-                'month_due',
-                'previous_report',
-                'progress',
-                'percent',
-                'papers',
-                'paper_submission_date',
-                'date_edited']]
+    dfnew = dfcurrent[['id', 'code', 'work_package',
+                       'description',
+                       'partner',
+                       'person_responsible',
+                       'month_due',
+                       'previous_report',
+                       'progress',
+                       'percent',
+                       'papers',
+                       'paper_submission_date',
+                       'date_edited']]
     month_due = dfnew.month_due
     month_due = month_due.replace(month_dic)
     dfnew['month_due'] = month_due

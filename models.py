36bb475143ef078eb32d7d1a96529673a6b78205
helen <h.l.burns@leeds.ac.uk>
2020-03-02 12:35:12 +0000

    :hammer_and_wrench: added the archive tables in

--- models.py
+++ models.py
@@ -50,6 +50,28 @@ class Work_Packages(db.Model):
         return '<id {}>'.format(self.id)
 
 
+class Work_Packages_Archive(db.Model):
+    __tablename__ = 'work_packages_archive'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date_edited = db.Column(db.Date())
+    code = db.Column(db.String(), nullable=False, unique=True)
+    status = db.Column(db.String())
+    issues = db.Column(db.String())
+    next_deliverable = db.Column(db.String())
+
+    def __init__(self, date_edited, code, status, issues,
+                 next_deliverable):
+        self.date_edited = date_edited
+        self.code = code
+        self.status = status
+        self.issues = issues
+        self.next_deliverable = next_deliverable
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
 class Deliverables(db.Model):
     __tablename__ = 'deliverables'
 
@@ -85,6 +107,34 @@ class Deliverables(db.Model):
         self.papers = papers
         self.paper_submission_date = paper_submission_date
         self.date_edited = date_edited
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class Deliverables_Archive(db.Model):
+    __tablename__ = 'deliverables_archive'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date_edited = db.Column(db.Date())
+    code = db.Column(db.String(), nullable=False, unique=True)
+    person_responsible = db.Column(db.String())
+    month_due = db.Column(db.Date, nullable=False)
+    progress = db.Column(db.String())
+    percent = db.Column(db.Integer, nullable=False)
+    papers = db.Column(db.String())
+    paper_submission_date = db.Column(db.Date())
+
+    def __init__(self, date_edited, code, person_responsible, month_due,
+                 progress, percent, papers, paper_submission_date):
+        self.date_edited = date_edited
+        self.code = code
+        self.person_responsible = person_responsible
+        self.month_due = month_due
+        self.progress = progress
+        self.percent = percent
+        self.papers = papers
+        self.paper_submission_date = paper_submission_date
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -166,6 +216,34 @@ class Tasks(db.Model):
         return '<id {}>'.format(self.id)
 
 
+class Tasks_Archive(db.Model):
+    __tablename__ = 'tasks_archive'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date_edited = db.Column(db.Date())
+    code = db.Column(db.String(), nullable=False, unique=True)
+    person_responsible = db.Column(db.String())
+    month_due = db.Column(db.Date, nullable=False)
+    progress = db.Column(db.String())
+    percent = db.Column(db.Integer, nullable=False)
+    papers = db.Column(db.String())
+    paper_submission_date = db.Column(db.Date())
+
+    def __init__(self, date_edited, code, person_responsible, month_due,
+                 progress, percent, papers, paper_submission_date):
+        self.date_edited = date_edited
+        self.code = code
+        self.person_responsible = person_responsible
+        self.month_due = month_due
+        self.progress = progress
+        self.percent = percent
+        self.papers = papers
+        self.paper_submission_date = paper_submission_date
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
 class Users2Partners(db.Model):
     __tablename__ = 'users2partners'
 
@@ -180,6 +258,21 @@ class Users2Partners(db.Model):
     def __init__(self, username, partner):
         self.username = username
         self.partner = partner
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class Counts(db.Model):
+    __tablename__ = 'counts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    code = db.Column(db.String(), nullable=False, unique=True)
+    counts = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, code, count):
+        self.code = code
+        self.count = count
 
     def __repr__(self):
         return '<id {}>'.format(self.id)

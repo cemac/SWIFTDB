7894f3d6a49ba9a366bc560199c24636d4619670
cemachelen <h.l.burns@leeds.ac.uk>
2020-06-08 16:44:24 +0100

    :hammer_and_wrench: minor tweaks

--- SWIFTDBApp.py
+++ SWIFTDBApp.py
@@ -683,7 +683,7 @@ def wp_summary(id):
     colnames = [s.replace("_", " ").title()
                 for s in data.columns.values[1:]]
     return render_template('view.html.j2', title=title, colnames=colnames,
-                           tableClass='Tasks', editLink="none", reader='False',
+                           tableClass='Tasks', editLink="none", reader='True',
                            data=data, description=description)
 # Tasks for a given user
 
